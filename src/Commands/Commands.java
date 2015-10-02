e827070980931c0e45b4eabedacb897ce095c80b
salowed <salowed@gmail.com>
2015-10-02 13:35:12 -0400

    Added basic insert stuff and numberbucket helper

--- src/Commands/Commands.java
+++ src/Commands/Commands.java
@@ -1,7 +1,9 @@
 package Commands;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.json.JSONException;
 
 import Manager.ClusterManager;
@@ -14,8 +16,21 @@ public class Commands {
 		return false;
 	}	
 
-	public static boolean insert(String cmd) {
-		// TODO Auto-generated method stub
+	/**
+	 * 
+	 * @param cmd
+	 * @return
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public static boolean insert(String cmd) throws IOException, JSONException {
+		String primaryKey = "";
+		
+		//TODO: entire method needs to be written
+		//Hash primary  key using SHA-1
+		String hashedValue = DigestUtils.sha1Hex(primaryKey);
+		int fileNumber = pickNumberBucket(ClusterManager.getNodes().size(), hashedValue);
+		
 		return false;
 	}
 	
@@ -72,4 +87,27 @@ public class Commands {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	/**
+	 * Picks which bucket hash value goes in
+	 * @param numWorkers - number of ranges to split
+	 * @param hashedValue - value to place
+	 * @return node that value hashes to
+	 */
+	private static int pickNumberBucket(int numWorkers, String hashedValue) {
+		String maxValue = "";
+		for(int i = 0; i < 40; i++){
+			maxValue += "f";
+		}
+		BigInteger hash = new BigInteger(hashedValue, 16);
+		BigInteger bigMax = new BigInteger(maxValue, 16).add(BigInteger.ONE);
+		
+		BigInteger rangeSize = bigMax.divide(BigInteger.valueOf(numWorkers));
+		
+		int bucket = hash.divide(rangeSize).intValue() + 1;
+		return bucket;
+	}
+
+	
+	
 }
